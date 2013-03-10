738aa205f7d930332e8d607e63a6a5a8a879aa00
Kevin Dinkel <dinkelk@gmail.com>
2013-03-10 17:42:02 -0600

    made sure all links work

--- js/application.js
+++ js/application.js
@@ -18,7 +18,19 @@ $(function () {
     $("#link-sign-up").fakeIt(".page-1",".page-2", function() {
        $("#link-sign-up").hide();
     });
+    $("#link-sign-up-1").fakeIt(".page-1",".page-2", function() {
+       $("#link-sign-up").hide();
+    });
+    $("#link-sign-up-2").fakeIt(".page-1",".page-2", function() {
+       $("#link-sign-up").hide();
+    });
+    $("#link-sign-up-3").fakeIt(".page-1",".page-2", function() {
+       $("#link-sign-up").hide();
+    });
     $("#link-home").fakeIt(".page-2",".page-1", function() {
+       $("#link-sign-up").show();
+    });
+    $("#link-back").fakeIt(".page-2",".page-1", function() {
        $("#link-sign-up").show();
     });
 });
